438c3302c8d8d3fb68e555ce42790c400d2df08c
marikben <70907667+marikben@users.noreply.github.com>
2021-03-28 20:16:46 +0300

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+import { Button, Header, Icon, Input, ListItem } from 'react-native-elements';
 
 export default function App() {
+  const [product, setProduct] = useState('');
+  const [amount, setAmount] = useState('');
+  const [item, setItem] = useState({product: '', amount: ''});
+  const [items, setItems] = useState([]);
+  
+  const saveList = () => {
+    setItem({product: product, amount: amount})
+    //console.log(item)
+    items.push(item)
+    //setItems([...items, {item}])
+    console.log(items)
+    setProduct('');
+    setAmount('');
+  }
+  
+  const renderItem = ({ items }) => (
+    <ListItem bottomDivider >
+      <ListItem.Content>
+        <ListItem.Title>{item.product}</ListItem.Title>
+        <ListItem.Subtitle>{item.amount}</ListItem.Subtitle>
+      </ListItem.Content>
+    </ListItem>
+  )
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Header
+      leftComponent={{ icon: 'menu', color: '#fff' }}
+      centerComponent={{ text: 'SHOPPING LIST', color: '#fff' }}
+      rightComponent={{ icon: 'home', color: '#fff' }}
+      />
+      <Input placeholder='Product' label='PRODUCT' onChangeText={product => setProduct(product)}
+      value={product}/>
+      <Input placeholder='Amount' label='AMOUNT' onChangeText={amount => setAmount(amount)}
+      value={amount}/>
+      <Button raised icon={{name: 'save'}} onPress={saveList} title="SAVE" />
+      <FlatList
+        data={items}
+        renderItem={renderItem}
+        keyExtractor={(item, index) => index.toString()}
+      />
     </View>
   );
 }
@@ -19,3 +55,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+
