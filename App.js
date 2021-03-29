783430aef77dcaafde5e911bcd883ab46fbb4dfc
marikben <70907667+marikben@users.noreply.github.com>
2021-03-29 19:07:46 +0300

    android build

--- App.js
+++ App.js
@@ -6,48 +6,57 @@ export default function App() {
   const [product, setProduct] = useState('');
   const [amount, setAmount] = useState('');
   const [items, setItems] = useState([]);
-  
+  const [length, setLength] = useState(0);
+
   const saveList = () => {
-    items.push({product: product, amount: amount})
+    items.push({id: length, product: product, amount: amount})
     //setItems([...items, {item}])
-    console.log(items)
+    //console.log(items)
     setProduct('');
     setAmount('');
+    setLength(length+1);
   };
 
   const remove = (id) => {
     setItems(items.filter(item => item.id !== id));
+    setLength(length+1); //to avoid making the same id twice
   };
 
   const renderItem = ({ item }) => (
+    <View>
     <ListItem bottomDivider >
       <ListItem.Content>
-        <ListItem.Title>{item.product}</ListItem.Title>
-        <ListItem.Subtitle>{item.amount}</ListItem.Subtitle>
-        <Text><Icon name='delete' color='#FF0000' onPress={() => remove(item.id)}/></Text>
+        <ListItem.Title style={{fontSize: 18}}>{item.product}</ListItem.Title>
+        <ListItem.Subtitle style={{fontSize: 16}}>{item.amount}</ListItem.Subtitle>
+        <Text style={{marginLeft: 320, marginTop: -40}} onPress={() => remove(item.id)}>
+          <Icon name="delete" size={30} color="#e33057" />
+        </Text>
       </ListItem.Content>
     </ListItem>
+    </View>
     
   )
-
+    
   return (
     <View style={styles.container}>
       <Header
       leftComponent={{ icon: 'menu', color: '#fff' }}
-      centerComponent={{ text: 'SHOPPING LIST', color: '#fff' }}
+      centerComponent={<Text style={{color: '#fff'}}>SHOPPING LIST</Text>} //used <Text> instead of {text} to get the white color
       rightComponent={{ icon: 'home', color: '#fff' }}
       />
       <Input placeholder='Product' label='PRODUCT' onChangeText={product => setProduct(product)}
       value={product}/>
       <Input placeholder='Amount' label='AMOUNT' onChangeText={amount => setAmount(amount)}
       value={amount}/>
-      <Button raised icon={{name: 'save'}} onPress={saveList} title="SAVE" />
-
-    <FlatList
-      keyExtractor={keyExtractor = (item, index) => index.toString()}
-      data={items}
-      renderItem={renderItem}
-    />
+      <View style={styles.button}>
+      <Button raised icon={{name: 'save', color: '#fff'}} onPress={saveList} title="SAVE" />
+      </View>
+      <Text style={styles.note}>Double click the bin to remove the item</Text>
+      <FlatList
+        keyExtractor={keyExtractor = (item, index) => index.toString()}
+        data={items}
+        renderItem={renderItem}
+      />
     </View>
   );
 }
@@ -56,7 +65,19 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff'
+   
   },
+  button: {
+    height: 100, 
+    width: 150,
+    marginLeft: 110,
+    marginRight: 110
+  },
+  note:{
+    color: 'grey',
+    marginTop: -50,
+    marginLeft: 10
+  }
 });
 
 
